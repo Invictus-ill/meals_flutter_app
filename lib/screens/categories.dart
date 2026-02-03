5e73d06eb444996cda6b5c727f5fcf5609d37cb6
Rohan Maheshwari <77726890+Invictus-ill@users.noreply.github.com>
2026-02-03 19:58:33 +0530

    Add animations to the screen (#1)
    * Adding explicit animation
    
    * Add animations to meal details and item screens with Hero and AnimatedSwitcher

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,16 +5,43 @@ import 'package:meals_flutter_app/models/meal.dart';
 import 'package:meals_flutter_app/screens/meals.dart';
 import 'package:meals_flutter_app/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeals});
 
   final List<Meal> availableMeals;
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+    _animationController.forward();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   void _selectCategory(BuildContext context, Category selectedCategory) {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (context) => MealsScreen(
-          meals: availableMeals
+          meals: widget.availableMeals
               .where(
                 (element) => element.categories.contains(selectedCategory.id),
               )
@@ -27,23 +54,37 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: EdgeInsets.all(24),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 2,
-        crossAxisSpacing: 16,
-        mainAxisSpacing: 16,
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: EdgeInsets.all(24),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+        ),
+        children: [
+          for (final Category category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            ),
+        ],
       ),
-      children: [
-        for (final Category category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              _selectCategory(context, category);
-            },
+      builder: (ctx, child) {
+        return SlideTransition(
+          position: Tween(begin: Offset(0, 0.5), end: Offset(0, 0)).animate(
+            CurvedAnimation(
+              parent: _animationController,
+              curve: Curves.decelerate,
+            ),
           ),
-      ],
+          child: child,
+        );
+      },
     );
   }
 }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -35,11 +35,14 @@ class MealItem extends StatelessWidget {
         elevation: 2,
         child: Stack(
           children: [
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
-              height: 200,
-              width: double.infinity,
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                height: 200,
+                width: double.infinity,
+              ),
             ),
             Positioned(
               bottom: 0,
